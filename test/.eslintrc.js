c3d9e12976e50498a41e4225acd3880a17966628
overlookmotel <theoverlookmotel@gmail.com>
2019-01-28 02:04:38 +0000

    Use `@overlookmotel/eslint-config-tests` ESLint config for tests

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,14 +1,7 @@
 'use strict';
 
 module.exports = {
-	env: {
-		mocha: true
-	},
-	plugins: [
-		'chai-friendly'
-	],
-	rules: {
-		'no-unused-expressions': ['off'],
-		'chai-friendly/no-unused-expressions': ['error']
-	}
+	extends: [
+		'@overlookmotel/eslint-config-tests'
+	]
 };
